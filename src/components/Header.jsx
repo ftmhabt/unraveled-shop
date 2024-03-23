2b1091d217f049ec446d7a3e5eb96f74cea15f10
ftmhabt <ftmh.a78@gmail.com>
2024-03-23 16:34:22 +0330

    Add header base & style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,14 @@
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className="flex px-[7rem] py-[1rem] gap-[2rem] *:cursor-pointer  hover:*:text-rose-700">
+        <div>sale</div>
+        <div>clothing</div>
+        <div>bags</div>
+        <div>accessories</div>
+        <div>about</div>
+        <div className="ml-auto">cart</div>
+        <div>account</div>
+        <div>search</div>
+    </div>
   )
 }
