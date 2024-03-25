3519484a76d284b0b97c566bc74eb548adc99d60
ftmhabt <ftmh.a78@gmail.com>
2024-03-25 20:01:40 +0330

    Filter products with filter bar

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,6 @@
-import { prototype } from "postcss/lib/previous-map";
 import PropTypes from "prop-types";
 
-export default function Products({ data, error, loading }) {
+export default function Products({ data, error, loading, category }) {
   return (
     <div className="flex justify-center flex-wrap gap-20 items-end">
       {(loading && (
@@ -9,23 +8,27 @@ export default function Products({ data, error, loading }) {
       )) ||
         (error && <div className="text-red-700">{error}</div>) ||
         (data &&
-          data.map((product) => (
-            <div
-              className="flex flex-col w-[250px] h-[350px] gap-5"
-              key={product.id}
-            >
-              <div className="flex items-center w-[200px] h-[250px]">
-                <img
-                  className="max-w-[200px] max-h-[250px]"
-                  src={product.image}
-                  alt={product.title}
-                />
+          data
+            .filter((product) =>
+              category === "all" ? product : product.category === category
+            )
+            .map((product) => (
+              <div
+                className="flex flex-col w-[250px] h-[350px] gap-5"
+                key={product.id}
+              >
+                <div className="flex items-center w-[200px] h-[250px]">
+                  <img
+                    className="max-w-[200px] max-h-[250px]"
+                    src={product.image}
+                    alt={product.title}
+                  />
+                </div>
+                <div className="overflow-hidden max-h-[70px]">
+                  {product.title}
+                </div>
               </div>
-              <div className="overflow-hidden max-h-[70px]">
-                {product.title}
-              </div>
-            </div>
-          )))}
+            )))}
     </div>
   );
 }
@@ -34,4 +37,5 @@ Products.propTypes = {
   data: PropTypes.arrayOf(PropTypes.object),
   error: PropTypes.string,
   loading: PropTypes.bool,
+  category: PropTypes.string,
 };
