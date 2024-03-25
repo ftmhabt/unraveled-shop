7a3266a99f0c77c77664dc195c2d6e29725e3c2b
ftmhabt <ftmh.a78@gmail.com>
2024-03-25 18:56:08 +0330

    Lift up states

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import FilterBar from "./FilterBar";
 import SortBar from "./SortBar";
 import Products from "./Products";
@@ -7,13 +7,39 @@ export default function ProductList() {
   const [category, SetCategory] = useState('all');
   const [sort, SetSort] = useState('newest');
 
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function FetchData() {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        if (!response.ok) {
+          throw new Error(`HTTP error: Status ${response.status}`);
+        }
+        let postsData = await response.json();
+        setData(postsData);
+        setError(null);
+      } catch (err) {
+        setData(null);
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    FetchData();
+  }, []);
+
   return (
     <div>
       <div>
         <FilterBar category={category} onChange={SetCategory} />
         <SortBar sort={sort} onChange={SetSort} />
       </div>
-      <Products category={category} sort={sort}/>
+      <Products data={data} loading={loading} error={error} category={category} sort={sort}/>
     </div>
   );
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,44 +1,37 @@
-import { useState, useEffect } from "react";
+import { prototype } from "postcss/lib/previous-map";
+import PropTypes from "prop-types";
 
-export default function Products() {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    async function FetchData() {
-      try {
-        const response = await fetch("https://fakestoreapi.com/products");
-        if (!response.ok) {
-          throw new Error(`HTTP error: Status ${response.status}`);
-        }
-        let postsData = await response.json();
-        setData(postsData);
-        setError(null);
-      } catch (err) {
-        setData(null);
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    FetchData();
-  }, []);
-
+export default function Products({ data, error, loading }) {
   return (
     <div className="flex justify-center flex-wrap gap-20 items-end">
-        {
-            (loading && <div className="text-xl font-medium">loading posts...</div>) ||
-            (error && <div className="text-red-700">{error}</div>) ||
-            (data && data.map((product) => <div className="flex flex-col w-[250px] h-[350px] gap-5" key={product.id}>
-                <div className="flex items-center w-[200px] h-[250px]">
-                <img className="max-w-[200px] max-h-[250px]" src={product.image} alt={product.title} />
-                </div>
-                <div className="overflow-hidden max-h-[70px]">{product.title}</div>
-            </div>))
-        }
+      {(loading && (
+        <div className="text-xl font-medium">loading posts...</div>
+      )) ||
+        (error && <div className="text-red-700">{error}</div>) ||
+        (data &&
+          data.map((product) => (
+            <div
+              className="flex flex-col w-[250px] h-[350px] gap-5"
+              key={product.id}
+            >
+              <div className="flex items-center w-[200px] h-[250px]">
+                <img
+                  className="max-w-[200px] max-h-[250px]"
+                  src={product.image}
+                  alt={product.title}
+                />
+              </div>
+              <div className="overflow-hidden max-h-[70px]">
+                {product.title}
+              </div>
+            </div>
+          )))}
     </div>
-    
   );
 }
+
+Products.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object),
+  error: PropTypes.string,
+  loading: PropTypes.bool,
+};
