53c371089ec74cc1593b6208347ea364e3c794d1
ftmhabt <ftmh.a78@gmail.com>
2024-04-02 20:43:16 +0330

    Add add-to-cart button to product single page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,20 +1,42 @@
 import { useParams } from "react-router-dom";
 import { useContext } from "react";
 import { ShopContext } from "../context/Context";
+import { toast } from "react-toastify";
 
 export default function Product() {
-  const { data } = useContext(ShopContext);
+  const { cart, data, addToCart } = useContext(ShopContext);
   const { id } = useParams();
   if (!data) {
     return <div>Loading product...</div>;
   }
 
-  let product = data.find((item) => (item?.id) === Number(id));
+  let product = data.find((item) => item?.id === Number(id));
+
+  const getCartQuantity = (cart, productId) => {
+    const item = cart.find((item) => item.id === productId);
+    return item ? `(${item.quantity})` : "";
+  };
 
   return (
-    <div>
-      <div>{product?.title}</div>
-      <img src={product?.image} alt={product?.title} />
+    <div className="flex p-[2rem] gap-[2rem]">
+      <img
+        className="max-w-[250px]"
+        src={product?.image}
+        alt={product?.title}
+      />
+      <div className="flex flex-col items-start gap-[2rem]">
+        <div className="font-bold">{product?.title}</div>
+        <div>{product?.description}</div>
+        <button
+          className="bg-red-800 px-[2rem] py-[.5rem] text-white"
+          onClick={() => {
+            addToCart(product.id);
+            toast.success("added to cart!");
+          }}
+        >
+          add to cart {getCartQuantity(cart, product.id)}
+        </button>
+      </div>
     </div>
   );
 }
