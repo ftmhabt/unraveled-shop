293d7b7ff0e580ad461c9078cb4b5fca6401f805
ftmhabt <ftmh.a78@gmail.com>
2024-05-02 13:08:36 +0330

    Improve with ScrollRestoration

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,5 @@
-import { useParams } from "react-router-dom";
-import { useContext,useEffect } from "react";
+import { useParams, ScrollRestoration } from "react-router-dom";
+import { useContext, useEffect } from "react";
 import { ShopContext } from "../context/Context";
 import { toast } from "react-toastify";
 
@@ -10,37 +10,38 @@ export default function Product() {
     return <div>Loading product...</div>;
   }
 
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
-
   let product = data.find((item) => item?.id === Number(id));
 
   const getCartQuantity = (cart, productId) => {
-    const item = cart.find((item) => item.id === productId && item.quantity>0);
+    const item = cart.find(
+      (item) => item.id === productId && item.quantity > 0
+    );
     return item ? `(${item.quantity})` : "";
   };
 
   return (
-    <div className="flex p-[2rem] gap-[2rem] lg:flex-nowrap flex-wrap">
-      <img
-        className="max-w-[250px]"
-        src={product?.image}
-        alt={product?.title}
-      />
-      <div className="flex flex-col items-start gap-[2rem]">
-        <div className="font-bold">{product?.title}</div>
-        <div>{product?.description}</div>
-        <button
-          className="bg-red-800 px-[2rem] py-[.5rem] text-white"
-          onClick={() => {
-            addToCart(product.id);
-            toast.success("added to cart!");
-          }}
-        >
-          add to cart {getCartQuantity(cart, product.id)}
-        </button>
+    <>
+      <div className="flex p-[2rem] gap-[2rem] lg:flex-nowrap flex-wrap">
+        <img
+          className="max-w-[250px]"
+          src={product?.image}
+          alt={product?.title}
+        />
+        <div className="flex flex-col items-start gap-[2rem]">
+          <div className="font-bold">{product?.title}</div>
+          <div>{product?.description}</div>
+          <button
+            className="bg-red-800 px-[2rem] py-[.5rem] text-white"
+            onClick={() => {
+              addToCart(product.id);
+              toast.success("added to cart!");
+            }}
+          >
+            add to cart {getCartQuantity(cart, product.id)}
+          </button>
+        </div>
       </div>
-    </div>
+      <ScrollRestoration />
+    </>
   );
 }
